b60bb74114d5f10f90a60599e26fda6b4ec9d616
behdin123 <behdin2010@gmail.com>
2020-12-09 11:54:49 +0100

    new updates

--- JS/Main-Javascript.js
+++ JS/Main-Javascript.js
@@ -2,7 +2,7 @@
 
 const toggleButton = document.getElementsByClassName('toggleButton')[0]
         const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-        console.log(toggleButton);
+        //console.log(toggleButton);
 
 
 
@@ -10,3 +10,18 @@ const toggleButton = document.getElementsByClassName('toggleButton')[0]
         navbarLinks.classList.toggle('show')
         console.log(navbarLinks.classList);
         })
+
+
+
+const voice = document.querySelectorAll(".voice");
+const audio = document.querySelectorAll(".read-aloud");
+
+for(let i = 0; i < voice.length; i++){
+        voice[i].addEventListener("click", function () {
+                if(audio[i].style.display == "block"){
+                        audio[i].style.display="none";
+                }else{
+                        audio[i].style.display="block"; 
+                }
+        })
+}
